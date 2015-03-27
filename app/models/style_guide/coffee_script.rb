82bea3a35b36a5c9ae62d200b4062441c3d0e1bf
Teo Ljungberg <teo@teoljungberg.com>
2015-03-27 15:50:04 +0100

    Re-add "Lint `coffee.erb` files"
    This reverts commit e6d3261fab9f5a64f5635a82347efba3ddae4a00.

--- app/models/style_guide/coffee_script.rb
+++ app/models/style_guide/coffee_script.rb
@@ -4,7 +4,8 @@ module StyleGuide
     DEFAULT_CONFIG_FILENAME = "coffeescript.json"
 
     def violations_in_file(file)
-      Coffeelint.lint(file.content, config).map do |violation|
+      content = content_for_file(file)
+      lint(content).map do |violation|
         line = file.line_at(violation["lineNumber"])
 
         Violation.new(
@@ -18,6 +19,26 @@ module StyleGuide
     end
 
     private
+
+    def lint(content)
+      Coffeelint.lint(content, config)
+    end
+
+    def content_for_file(file)
+      if erb? file
+        begin
+          ERB.new(file.content).result
+        rescue
+          ""
+        end
+      else
+        file.content
+      end
+    end
+
+    def erb?(file)
+      file.filename.ends_with? ".erb"
+    end
 
     def config
       default_config.merge(repo_config.for(name))
