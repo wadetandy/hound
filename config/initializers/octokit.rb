64d538f42169e96815a7a5a16fa87b44a2922618
Wade Tandy <wtandy@bloomberg.net>
2015-05-08 12:20:35 -0400

    get working with bbgithub

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,12 +1,13 @@
-# require 'octokit'
-#
-# Octokit.configure do |c|
-#   c.api_endpoint = 'https://bbgithub.dev.bloomberg.com/api/v3'
-#   c.web_endpoint = 'https://bbgithub.dev.bloomberg.com'
-# end
-#
-# class Octokit::Client
-#   def user_teams(options = {})
-#     paginate "/api/v3/user/teams", options
-#   end
-# end
+require 'octokit'
+
+Octokit.configure do |c|
+  c.api_endpoint = 'https://bbgithub.dev.bloomberg.com/api/v3'
+  c.web_endpoint = 'https://bbgithub.dev.bloomberg.com'
+  c.proxy = 'http://bproxy.cfe.bloomberg.com'
+end
+
+class Octokit::Client
+  def user_teams(options = {})
+    paginate "/api/v3/user/teams", options
+  end
+end
--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -3,11 +3,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :github,
     ENV['GITHUB_CLIENT_ID'],
     ENV['GITHUB_CLIENT_SECRET'],
-    # client_options: {
-    #   site: 'https://bbgithub.dev.bloomberg.com/api/v3',
-    #   authorize_url: 'https://bbithub.dev.bloomberg.com/login/oauth/authorize',
-    #   token_url: 'https://bbithub.dev.bloomberg.com/login/oauth/access_token'
-    # },
+    client_options: {
+      site: 'https://bbgithub.dev.bloomberg.com/api/v3',
+      authorize_url: 'https://bbgithub.dev.bloomberg.com/login/oauth/authorize',
+      token_url: 'https://bbgithub.dev.bloomberg.com/login/oauth/access_token',
+      connection_opts: {
+        proxy: 'http://bproxy.cfe.bloomberg.com'
+      }
+    },
     scope: 'user:email,repo'
   )
 end
